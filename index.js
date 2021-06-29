51f83f6b878a650f202d17088783925507a5c59e
Ankush Sharma <84198124+Ankushsarma@users.noreply.github.com>
2021-06-29 16:48:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ circle.classList.add("cirlce_active");
 main_1.classList.add( "main_2");
 welcome_h1.classList.add("welcome_h1_opacity");
 con11.classList.add("con11_left_2");
+welcome_h1.classList.add("welecome_h1_padinf");
 
 circle.style.transition = "all 1s";
 main_1.style.transition = "all 1s";
@@ -18,42 +19,46 @@ con11.style.transition = "all 1s";
 
 
 
-
+myFunction();
 text_o();
-
-myFunction()
-welcome_h1.classList.add("welecome_h1_padinf");
-
-
 welcome();
+removing();
+
 }
+
+
 
 function myFunction() {
   setTimeout(function (){
       window.open("home.html");
-  }, 2000
-
-  )
-
-}
+  }, 1500
+)}
 
 
 function text_o() {
-
-
   setTimeout(
     function(){
       welcome_h1.classList.remove("welcome_h1_opacity");
     } , 700
-  )
-}
+  )}
 
 
 function welcome(){
   setTimeout( function(){
     document.querySelector(".welcome_h1").innerText="welcome"
   } , 300
+)}
 
-  )
 
-}
+
+
+
+function removing(){
+  setTimeout(function(){
+    circle.classList.remove("cirlce_active");
+    main_1.classList.remove( "main_2");
+    welcome_h1.classList.remove("welcome_h1_opacity");
+    con11.classList.remove("con11_left_2");
+    welcome_h1.classList.remove("welecome_h1_padinf");
+  } , 1800
+)}
